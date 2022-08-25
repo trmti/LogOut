8f981e4e9f23906ae00533f0b07a01cd7d353e1c
trmti <trmtikko@gmail.com>
2022-08-25 10:54:36 +0900

    change sql

--- jsonServer.ts
+++ jsonServer.ts
@@ -12,9 +12,19 @@ const connection = await pool.connect();
 try {
   // Create the table
   await connection.queryObject`
-    CREATE TABLE IF NOT EXISTS todos (
+    CREATE TABLE IF NOT EXISTS nftMetaDatas (
+      tokenId SERIAL PRIMARY KEY,
+      name TEXT NOT NULL,
+      description TEXT NOT NULL,
+      image TEXT NOT NULL,
+    )
+  `;
+  await connection.queryObject`
+    CREATE TABLE IF NOT EXISTS nftPersonalDatas (
       id SERIAL PRIMARY KEY,
-      title TEXT NOT NULL
+      tokenId SERIAL REFERENCES nftMetaDatas(tokenId),
+      level INTEGER NOT NULL,
+      damages INTEGER[][2] NOT NULL,
     )
   `;
 } finally {
@@ -23,62 +33,24 @@ try {
 }
 
 serve(async (req) => {
-  // Parse the URL and check that the requested endpoint is /todos. If it is
-  // not, return a 404 response.
   const url = new URL(req.url);
-  if (url.pathname !== '/todos') {
-    return new Response('Not Found', { status: 404 });
-  }
-
-  // Grab a connection from the database pool
   const connection = await pool.connect();
 
   try {
-    switch (req.method) {
-      case 'GET': {
-        // This is a GET request. Return a list of all todos.
-        // Run the query
-        const result = await connection.queryObject`
-          SELECT * FROM todos
+    switch (url.pathname) {
+      case '/nft': {
+        const nftMetaDatas = connection.queryObject`
+          SELECT * FROM nftMetaDatas
         `;
-
-        // Encode the result as JSON
-        const body = JSON.stringify(result.rows, null, 2);
-
-        // Return the result as JSON
-        return new Response(body, {
-          headers: { 'content-type': 'application/json' },
-        });
+        return new Response(nftMetaDatas, { status: 200 });
       }
-      case 'POST': {
-        // This is a POST request. Create a new todo.
-        // Parse the request body as JSON. If the request body fails to parse,
-        // is not a string, or is longer than 256 chars, return a 400 response.
-        const title = await req.json().catch(() => null);
-        if (typeof title !== 'string' || title.length > 256) {
-          return new Response('Bad Request', { status: 400 });
-        }
-
-        // Insert the new todo into the database
-        await connection.queryObject`
-          INSERT INTO todos (title) VALUES (${title})
-        `;
-
-        // Return a 201 Created response
-        return new Response('', { status: 201 });
-      }
-      default:
-        // If this is neither a POST, or a GET return a 405 response.
-        return new Response('Method Not Allowed', { status: 405 });
     }
-  } catch (err) {
-    console.error(err);
-    // If an error occurs, return a 500 response
-    return new Response(`Internal Server Error\n\n${err.message}`, {
-      status: 500,
+  } catch {
+    return new Response('Not Found', {
+      status: 404,
     });
   } finally {
-    // Release the connection back into the pool
     connection.release();
   }
+  return new Response('not Found', { status: 404 });
 });
