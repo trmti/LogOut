bcd5b93ae60f8ccddc988278533c345bde65f4b1
trmti <trmtikko@gmail.com>
2022-08-27 21:26:49 +0900

    test

--- mintServer.ts
+++ mintServer.ts
@@ -69,34 +69,60 @@ if (MINTER_PRIVATE_KEY && GNTOKEN_ADDRESS && PROVIDER_URL && BOOSTTOKEN_ADDRESS 
               const now = Date.now();
               const nowDate = new Date(now);
               const formatedDate = nowDate.getFullYear() + '-' + nowDate.getMonth() + '-' + nowDate.getDate() + '-';
-              await fetch(`${JSON_SERVER_URL}/personalData/addSleepLog`, {
-                  method: "POST",
-                  headers: {'Content-Type': 'application/json'},
-                  body: JSON.stringify({id: params.nftId, duration: params.sleepDuration, date: formatedDate})
-                }
-              )
+
               const nftMetaData = await (await fetch(`${JSON_SERVER_URL}/NFTJsonData?id=${params.id}`, {
                 method: "GET"
               })).json();
               const sleepLog = nftMetaData.sleeps;
               console.log(sleepLog);
-              let durations = 0;
-              for (let i = sleepLog.length-1; i > 0; i--) {
-                const date = new Date(sleepLog.date);
-                const lastDate = Object.assign(nowDate, {});
-                if (date < lastDate) {
-                  break;
-                } else {
-                  console.log(sleepLog[i].duration)
-                  durations += Number(sleepLog[i].duration)
+              const latestDate = new Date(sleepLog[sleepLog.length - 1].date.split(".")[0])
+              if (
+                sleepLog.length == 0 ||
+                latestDate.getFullYear() < nowDate.getFullYear() ||
+                (
+                  latestDate.getFullYear() == nowDate.getFullYear() &&
+                  latestDate.getMonth() < nowDate.getMonth()
+                ) ||
+                (
+                  latestDate.getFullYear() == nowDate.getFullYear() &&
+                  latestDate.getMonth() == nowDate.getMonth() &&
+                  latestDate.getDate() < nowDate.getDate()
+                )
+              ) {
+                let durations = 0;
+                for (let i = sleepLog.length-1; i > 0; i--) {
+                  const date = new Date(sleepLog.date);
+                  const lastDate = Object.assign(nowDate, {});
+                  if (date < lastDate) {
+                    break;
+                  } else {
+                    durations += Number(sleepLog[i].duration)
+                  }
                 }
+                const res = await fetch(`${JSON_SERVER_URL}/calculateMintVol`, {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json"
+                  },
+                  body: JSON.stringify({personalId: params.id, sleepDuration: (durations/6 + params.sleepDuration)/2})
+                })
+                const mintAmount = (await res.json()).vol;
+                
+                console.log(mintAmount)
+                // await fetch(`${JSON_SERVER_URL}/personalData/addSleepLog`, {
+                //     method: "POST",
+                //     headers: {'Content-Type': 'application/json'},
+                //     body: JSON.stringify({id: params.nftId, duration: params.sleepDuration, date: formatedDate})
+                //   })
+                  return new Response("mint Successfully!!", {status: 200})
+                }
+                else {
+                  return new Response("You already inserted sleepLog", {status: 400})
+                }
+              } catch(e) {
+                console.error(e);
+                return new Response("Internal Server Error", {status: 400})
               }
-              console.log(durations)
-              return new Response("mint Successfully!!", {status: 200})
-            } catch(e) {
-              console.error(e);
-              return new Response("Internal Server Error", {status: 400})
-            }
           } else {
             return new Response(
               'Insert Value Failed. You may added invalid params',
