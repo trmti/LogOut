ee6eaeccb039d01c1e2dc6313345002381a8aba6
trmti <trmtikko@gmail.com>
2022-08-27 19:19:13 +0900

    test

--- jsonServer.ts
+++ jsonServer.ts
@@ -238,7 +238,7 @@ serve(async (req) => {
           const x =
             mintVol[~~(params.sleepDuration / 2)][
               ~~((levelJson.level - 1) / 5)
-            ] / 96;
+            ];
           console.log(x);
           const gamma = (1 / 362880) * x ** 9 * Math.E ** x;
           return new Response(
