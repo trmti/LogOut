3718c7bba03beb13d9a60e5f6d43c99327a46ba2
trmti <trmtikko@gmail.com>
2022-08-30 11:23:08 +0900

    test

--- mintServer.ts
+++ mintServer.ts
@@ -6,7 +6,7 @@ import GoodNightABI from "./GoodNightToken.json" assert { type: "json" }
 import BoostNFTABI from "./BoostNFT.json" assert { type: "json" }
 
 const MINTER_PRIVATE_KEY = Deno.env.get("MINTER_PRIVATE_KEY");
-const GNTOKEN_ADDRESS = Deno.env.get("GNTOKEN_ADDRESS")
+const GNTOKEN_ADDRESS = Deno.env.get("GNTOKEN_ADDRESS");
 const PROVIDER_URL = Deno.env.get("PROVIDER_URL")
 const BOOSTTOKEN_ADDRESS = Deno.env.get("BOOSTTOKEN_ADDRESS")
 const JSON_SERVER_URL = Deno.env.get("JSON_SERVER_URL")
